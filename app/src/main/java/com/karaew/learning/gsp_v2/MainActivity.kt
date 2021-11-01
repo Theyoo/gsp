c9d0a31c480cd4fb2f71334e56353dc1a9ea1d31
Theyoo <andreyka.karaev.1999@mail.ru>
2021-11-01 19:19:19 +0300

    update theme

--- app/src/main/java/com/karaew/learning/gsp_v2/MainActivity.kt
+++ app/src/main/java/com/karaew/learning/gsp_v2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.karaew.learning.gsp_v2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
     }
 
